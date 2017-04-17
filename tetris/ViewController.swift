672a812297bf1796c67bc67f719a04ed2b05ba67
加藤 舞子 <katoumaiko@maikos-macbook-pro.local>
2017-04-17 19:27:45 +0900

    Initial Commit

---
+++ tetris/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tetris
+//
+//  Created by 加藤 舞子 on 2017/04/17.
+//  Copyright © 2017年 加藤 舞子. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
